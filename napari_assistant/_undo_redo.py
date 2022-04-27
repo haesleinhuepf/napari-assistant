9484b0fbc4dfd3fa073f402db8ec156b03edb930
Cryaaa <ryan.savill@web.de>
2022-04-27 17:00:14 +0200

    Undo Redo now working
    Some progress has already been made in trying to make these steps faster for very big workflows but are not done yet and labelled experimental accordingly

--- napari_assistant/_undo_redo.py
+++ napari_assistant/_undo_redo.py
@@ -1,5 +1,5 @@
 from napari_assistant._workflow_io_utility import load_remaining_workflow, initialise_root_functions
-from napari_workflows import WorkflowManager
+from napari_workflows import WorkflowManager, Workflow
 from napari import Viewer
 
 def delete_workflow_widgets_layers(viewer: Viewer):
@@ -11,7 +11,7 @@ def delete_workflow_widgets_layers(viewer: Viewer):
     viewer: napari.Viewer
     """
     # install the workflow manager and get the current workflow
-    manager = WorkflowManager.install(viewer)
+    manager: WorkflowManager = WorkflowManager.install(viewer)
     workflow = manager.workflow
     
     #deleting the widgets based on the workflow
@@ -28,31 +28,37 @@ def delete_workflow_widgets_layers(viewer: Viewer):
         if layers[layer_name].source.widget:
             layers.remove(layer_name)
 
-def undo(viewer: Viewer):
-    """
-    Deletes all layers which were created by widgets and closes the widgets prior
+def clear_and_load_workflow(viewer: Viewer,manager_workflow: Workflow, workflow_to_load: Workflow, button_size = 32):
+    delete_workflow_widgets_layers(viewer)
+    manager_workflow.clear()
 
-    Parameters
-    ----------
-    viewer: napari.Viewer
-    """
-    # install the workflow manager and get the current workflow and controller
-    manager = WorkflowManager.install(viewer)
-    workflow = manager.workflow
-    controller = manager.undo_redo_controller
-    
-    
-    # only reload if there is an undo to be performed
-    if controller.undo_stack:
+    w_dw = initialise_root_functions(
+        workflow_to_load, 
+        viewer, 
+        button_size=button_size,
+    )
+    w_dw += load_remaining_workflow(
+        workflow_to_load, 
+        viewer,
+        button_size=button_size,
+    )
+    return w_dw
+            
+def change_widget_parameters(manager_workflow: Workflow, updated_workflow:Workflow):
+    from ._workflow_io_utility import category_kwargs, kwargs_of_wf_step
+    for key in manager_workflow._tasks.keys():
+        if manager_workflow._tasks[key] != updated_workflow._tasks[key]:
+            if key in manager_workflow.sources_of(key):
+                return # change input image
+            # make adapter and change widget parameters
+        widget = None # still need to find a way to access it!
+        # setting the right parameters
 
-        # undo workflow step: workflow is now the undone workflow
-        controller.undo()
+        cat_kwargs = category_kwargs(
+            updated_workflow._tasks[key][0],
+            kwargs_of_wf_step(updated_workflow,key),
+        )
+        for k,v in cat_kwargs.items():
+            widget[k].value = v
 
-        controller.freeze = True # no actions recorded or performed on workflow
-        delete_workflow_widgets_layers(viewer)
         
-        initialise_root_functions(workflow=workflow,
-                                  viewer=viewer)
-        load_remaining_workflow(workflow=workflow,
-                                viewer=viewer)
-        controller.freeze = False
