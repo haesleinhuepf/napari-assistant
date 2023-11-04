368f05d8596bb6c1f3e269e7194db298fd564dae
Robert Haase <haesleinhuepf@users.noreply.github.com>
2023-11-04 09:06:00 +0100

    remove/ignore deployment script

--- deploy.bat
+++ deploy.bat
@@ -4,8 +4,7 @@ call python setup.py sdist bdist_wheel
 
 call python -m pip install --user --upgrade twine
 
-call python -m twine upload --repository pypi dist/*
+set TWINE_USERNAME=__token__
+set TWINE_PASSWORD=pypi-AgEIcHlwaS5vcmcCJDEyYzM5MDBjLTg4ZmYtNDhmZC05ZTRiLTczMzc2MThlNjNlZAACKlszLCJmMzMyMmMwZi1iZTI0LTRiYWUtOTUxZS1hNGU5YmU2M2RlMWMiXQAABiDoX0JmgKtiQD_eJ5B6vSKJWeNDsP9i6JuzgYkwkF14dg
 
-
-
-
+call python -m twine upload dist/*
