db77c81d5bb2c1747e7714ddb296955a7d2af8a0
Robert Haase <haesleinhuepf@users.noreply.github.com>
2025-05-02 11:06:00 +0200

    fix menu discovery in napari 0.6.0

--- napari_assistant/__main__.py
+++ napari_assistant/__main__.py
@@ -1,4 +1,45 @@
 def main():
+    # napari 0.6.0 warns that plugins are potentially not working,
+    # and ask plugin developers to fix this.
+    # However, the plugins are working, they just broke the menu discovery.
+    # This code fixes the menu discovery and removes the warning.
+    def dummy(whatever):
+        pass
+    from napari._qt.qt_main_window import _QtMainWindow
+    _QtMainWindow._warn_on_shimmed_plugins = dummy
+
+    # import plugins ourself
+    try:
+        from importlib.metadata import entry_points
+
+        # Discover all napari plugins
+        try:
+            napari_plugins = entry_points(group='napari.plugin')
+        except TypeError:
+            all_plugins = entry_points()
+            try:
+                napari_plugins = all_plugins['napari.plugins']
+            except KeyError:
+                napari_plugins = []
+        print("plugins found:", len(napari_plugins))
+
+        # import all plugins
+        for ep in napari_plugins:
+            #print("loading", ep.name)
+            import time
+            start_time = time.time()
+            try:
+                ep.load()
+            except:
+                print(f"Error while loading plugin {ep.name}")
+            finally:
+                delta_time = time.time() - start_time
+                if delta_time > 0.1:
+                    print(f"Loading plugin {ep.name} took {delta_time} seconds")
+    except:
+        pass
+
+    # open a napari viewer
     import sys
     from ._viewer import Viewer
     from qtpy.QtCore import QTimer
